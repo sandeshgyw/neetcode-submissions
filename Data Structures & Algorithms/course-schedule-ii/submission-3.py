07368942896a398f4be14c6dafe9f1e62433342f
Sandesh Gyawali <40716486+sandeshgyw@users.noreply.github.com>
2026-06-02 06:49:15 -0500

    Add: course-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-3.py
@@ -0,0 +1,42 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        results=[]
+        adjList=defaultdict(list)
+        visited=set()
+        visiting=set()
+
+        for crs,pre in prerequisites:
+            adjList[crs].append(pre)
+
+        def canFinish(course):
+            if adjList[course]==[]:
+                if course not in results:
+                    results.append(course)
+                visited.add(course)
+                return True
+
+            if course in results:
+                return True
+
+            if course in visiting:
+                return False
+            
+            visiting.add(course)
+            for pre in adjList[course]:
+                if not canFinish(pre):
+                    return False
+            visiting.remove(course)
+            visited.add(course)
+            if course not in results:
+                results.append(course)
+            return True
+
+            
+
+        
+
+        for i in range(numCourses):
+            if not canFinish(i):
+                return []
+            
+        return (results)
