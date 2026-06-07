1dad78f49b16500928205b84993bf50d4d608b23
Sandesh Gyawali <40716486+sandeshgyw@users.noreply.github.com>
2026-06-07 18:36:56 -0500

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    cache={}
+    def climbStairs(self, n: int) -> int:
+    #if n reaches 0 it means we found one way to reach across all stairs
+    # if n reaches less than 0 means that path was not viable
+        if n==0:
+            return 1
+        if n<0:
+            return 0
+        
+        if n not in self.cache:
+            self.cache[n]=self.climbStairs(n-1)+self.climbStairs(n-2)
+        
+        return self.cache[n]
+        
+
+
+        
