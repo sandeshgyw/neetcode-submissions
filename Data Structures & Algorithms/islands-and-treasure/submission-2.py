2aabafb8c0ebddb737462afaa93d218168cf0660
Sandesh Gyawali <40716486+sandeshgyw@users.noreply.github.com>
2026-07-02 11:11:11 -0500

    Add: islands-and-treasure - submission-2

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-2.py
@@ -0,0 +1,44 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        #replce INF with the closest distance to a chest:0
+        #-1 is invalid i.e if we see -1 we dont need to traverse it
+
+        INF=2147483647
+        queue=deque([])
+
+        #lets first store all the gate indices
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]==0:
+                    queue.append((i,j))
+
+        #treasures has all the chests indices
+        level=0
+
+
+        while queue:
+            
+            for _ in range(len(queue)):
+                r,c=queue.popleft()
+                if r+1 <len(grid) and grid[r+1][c]==INF:
+                    grid[r+1][c]=grid[r][c]+1
+                    queue.append((r+1,c))
+                if r-1>=0 and grid[r-1][c]==INF:
+                    grid[r-1][c]=grid[r][c]+1
+                    queue.append((r-1,c))
+                if c+1 < len(grid[0]) and grid[r][c+1]==INF:
+                    grid[r][c+1]=grid[r][c]+1
+                    queue.append((r,c+1))
+                if c-1 >=0 and grid[r][c-1]==INF:
+                    grid[r][c-1]=grid[r][c]+1
+                    queue.append((r,c-1))
+            
+
+            
+                
+                    
+        
+    
+
+        
