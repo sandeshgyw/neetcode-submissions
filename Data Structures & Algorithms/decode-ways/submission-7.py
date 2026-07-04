686d502799df5c94f8efdfe0cde8b8e10a519c2d
Sandesh Gyawali <40716486+sandeshgyw@users.noreply.github.com>
2026-07-04 12:59:45 -0500

    Add: decode-ways - submission-7

---
+++ Data Structures & Algorithms/decode-ways/submission-7.py
@@ -0,0 +1,41 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        ways=0
+        cache={}
+
+        def backtrack(i):
+            nonlocal ways
+
+            if i in cache:
+                return cache[i]
+            if i>=len(s):
+                return 1
+            
+            if s[i]=='0':
+                return 0
+            
+            #single digit invlaid only if 0
+
+            ways+=backtrack(i+1)
+            
+            #now we enter 2 digit area
+
+            if s[i]=='0':
+                return 0
+
+            if i+1>=len(s):
+                return 1
+    
+
+            if s[i]=='1' or (s[i]=='2' and s[i+1] in '0123456'):
+                ways+=backtrack(i+2)
+            
+            cache[i]=ways
+            
+            return cache[i]
+        backtrack(0)
+
+        return ways
+            
+
+        
