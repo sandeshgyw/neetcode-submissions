44d7661b4e2c79f386746be765d1d8c1212476cb
Sandesh Gyawali <40716486+sandeshgyw@users.noreply.github.com>
2026-07-05 07:58:36 -0500

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,30 @@
+class MinStack:
+    stack,minStack=[],[]
+
+    def __init__(self):
+        self.stack=[]
+        self.minStack=[]
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.minStack:
+            self.minStack.append(val)
+        if val<self.minStack[-1]:
+            self.minStack.append(val)
+        
+
+    def pop(self) -> None:
+        self.stack.pop()
+        if self.minStack:
+            self.minStack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+        
