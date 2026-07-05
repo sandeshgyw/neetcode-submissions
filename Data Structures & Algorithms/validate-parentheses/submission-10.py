9689180a1fe7e078ca73e07f23b7589ff75a883d
Sandesh Gyawali <40716486+sandeshgyw@users.noreply.github.com>
2026-07-05 07:47:41 -0500

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+
+        openToClose={
+            '}':'{',
+            ')':'(',
+            ']':'['
+        }
+
+        for i in range(len(s)):
+            if s[i] not in openToClose:
+                #means its an open bracket
+                stack.append(s[i])
+            
+            else:
+                
+                if not stack or stack.pop()!=openToClose[s[i]]:
+                    return False
+        
+        if stack:
+            return False
+        
+        return True
+
+        
