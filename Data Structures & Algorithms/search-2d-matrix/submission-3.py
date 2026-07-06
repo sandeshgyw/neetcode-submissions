16caaf9671279892f777b9678da2803abca848d9
Sandesh Gyawali <40716486+sandeshgyw@users.noreply.github.com>
2026-07-06 08:33:09 -0500

    Add: search-2d-matrix - submission-3

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-3.py
@@ -0,0 +1,53 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        ROWS,COLS=len(matrix),len(matrix[0])
+
+        #find which row it is in
+        #cols are sorted so BS
+        #then in a row find where again using BS
+
+        if target <matrix[0][0] or target > matrix[ROWS-1][COLS-1]:
+            return False
+
+        l,r=0,ROWS-1
+        
+
+        while l<r:
+            mid=l+(r-l)//2
+
+            if matrix[mid][0]>target:
+                #row r has all grater than target
+                r=mid-1
+            
+            if matrix[mid][COLS-1]<target:
+                l=mid+1
+
+            if matrix[mid][0]<=target and matrix[mid][COLS-1]>=target:
+                l=r=mid
+            
+        
+
+        mat=matrix[l]
+
+        l,r=0,COLS-1
+
+        while l<r:
+            mid=l+(r-l)//2
+
+            if mat[mid]==target:
+                return True
+            
+            if mat[mid]<target:
+                l=mid+1
+            if mat[mid]>target:
+                r=mid-1
+        return False
+
+
+
+
+
+
+
+
+        
