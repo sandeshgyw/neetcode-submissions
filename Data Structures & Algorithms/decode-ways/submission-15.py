b1dbfae10b1541b28fbc77bf06b64d9013b898b3
Sandesh Gyawali <40716486+sandeshgyw@users.noreply.github.com>
2026-07-27 09:15:56 -0500

    Add: decode-ways - submission-15

---
+++ Data Structures & Algorithms/decode-ways/submission-15.py
@@ -0,0 +1,36 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        cache={}
+        
+
+        def decode(i):
+            #this functions gives
+            #total paths that reach the end validly
+            #or how many ways can we decode moving forward from this index
+            #valid path
+            if i>=len(s):
+                return 1
+
+            #invalid path
+            if s[i]=='0':
+                return 0
+            
+            if i in cache:
+                return cache[i]
+            
+            
+            ways=decode(i+1)
+            #if one digit is not 0 then we can move forward
+            #but for 2 digit, there are other conditions and only
+            #if that satisfies then we move forward
+
+            if i+1<len(s) and (s[i]=='1' or (s[i]=='2' and s[i+1] in '0123456')):
+                ways+=decode(i+2)
+            
+            cache[i]=ways
+            
+            return cache[i]
+        
+        return decode(0)
+
+        
