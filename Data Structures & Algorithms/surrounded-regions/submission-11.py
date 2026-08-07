05ef04bd1c12194e950232dea4c49e80ac2b9771
Sandesh Gyawali <40716486+sandeshgyw@users.noreply.github.com>
2026-08-07 10:00:58 -0500

    Add: surrounded-regions - submission-11

---
+++ Data Structures & Algorithms/surrounded-regions/submission-11.py
@@ -0,0 +1,42 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        ROWS,COLS=len(board),len(board[0])
+        border_o_cells=set()
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                #O(n^2)
+                if board[r][c]=='O' and (r==0 or r==ROWS-1 or c==0 or c==COLS-1):
+                    border_o_cells.add((r,c))
+            
+        
+        def dfs(r,c):
+            if r<0 or r>ROWS-1 or c<0 or c>COLS-1 or board[r][c]!='O':
+                return
+            
+            board[r][c]='T'
+
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+
+
+
+        for r,c in border_o_cells:
+            if board[r][c]=='O':
+                dfs(r,c)
+    
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c]=='O':
+                    board[r][c]='X'
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c]=="T":
+                    board[r][c]='O'
+        
+
+
+        
