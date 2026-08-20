c24ba344fe5ee23ad6a34113236a955a3d4195ea
Sandesh Gyawali <40716486+sandeshgyw@users.noreply.github.com>
2026-08-20 10:13:09 -0500

    Add: is-palindrome - submission-14

---
+++ Data Structures & Algorithms/is-palindrome/submission-14.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l,r=0,len(s)-1
+
+        while l<r:
+            while not s[l].isalnum():
+                l+=1
+            while not s[r].isalnum():
+                r-=1
+            
+            if s[l].upper()!=s[r].upper():
+                return False
+            l+=1
+            r-=1
+        
+        return True
+        
