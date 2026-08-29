3464ed69ac2324ca960dbaebc86b295f7027be7b
Sandesh Gyawali <40716486+sandeshgyw@users.noreply.github.com>
2026-08-29 15:22:45 -0500

    Add: lru-cache - submission-4

---
+++ Data Structures & Algorithms/lru-cache/submission-4.py
@@ -0,0 +1,48 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.k=capacity
+        self.cache={}
+        self.queue=[]
+        
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        
+        if key not in self.queue:
+            if len(self.queue)>=self.k:
+                val=self.queue.pop(0)
+                del self.cache[val]
+            self.queue.append(key)
+        else:
+            index=self.queue.index(key)
+            temp=self.queue.pop(index)
+            self.queue.append(temp)
+
+        return self.cache[key]
+        
+        
+
+    def put(self, key: int, value: int) -> None:
+
+        if key in self.cache:
+            index=self.queue.index(key)
+            temp=self.queue.pop(index)
+            self.cache[key]=value
+            self.queue.append(temp)
+            
+        else:
+            if len(self.queue)>=self.k:
+                temp=self.queue.pop(0)
+                del self.cache[temp]
+                self.cache[key]=value
+                self.queue.append(key)
+            else:
+                self.cache[key]=value
+                self.queue.append(key)
+        
+      
+
+
+        
