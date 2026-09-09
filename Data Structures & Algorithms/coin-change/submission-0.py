426865e07cb4ae63cfbfe1711c55bf2730a21197
Sandesh Gyawali <40716486+sandeshgyw@users.noreply.github.com>
2026-09-09 15:31:27 -0500

    Add: coin-change - submission-0

---
+++ Data Structures & Algorithms/coin-change/submission-0.py
@@ -0,0 +1,36 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        res=[]
+        result=[]
+    
+        def backtrack(i,total):
+            #this function checks if the coin makes the sum to amount or not
+            # and return -1 if invalid
+
+            if i==len(coins):
+                return -1
+            if total>amount:
+                return -1
+            if amount==total:
+                result.append(res.copy())
+            
+            res.append(coins[i])
+            backtrack(i,total+coins[i])
+            res.pop()
+            backtrack(i+1,total)
+        
+        backtrack(0,0)
+        minLength=float('+inf')
+        
+        for i in range(len(result)):
+            minLength=min(minLength,len(result[i]))
+
+        return minLength if result else -1
+                
+            
+
+            
+
+
+
+        
